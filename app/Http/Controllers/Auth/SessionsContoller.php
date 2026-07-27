8d23910472ffd68c5c01c83e591ee9139520a014
mihajlos2 <mihajlostevanovic2003@gmail.com>
2026-07-27 14:05:25 +0200

    Initial commits

---
+++ app/Http/Controllers/Auth/SessionsContoller.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+
+class SessionsContoller extends Controller
+{
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $user = $request->validate([
+            'email' => ['required', 'string','email', 'max:255'],
+            'password' => ['required', Password::default()],
+        ]);
+
+        if (Auth::attempt($user)) {
+            return redirect('/');
+        }else
+        {
+            return back()->withErrors([
+                'error' => 'Los unis emaila ili sifre',
+            ]);
+        }
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
